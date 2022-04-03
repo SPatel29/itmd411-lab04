e797a4fcae96715df433555ba25fecc10138cccb
SPatel29 <sunnypatelc9@gmail.com>
2022-04-03 13:50:16 -0500

    reformatted printf

--- src/LoanProcessing.java
+++ src/LoanProcessing.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.io.*;
 
 public class LoanProcessing extends BankRecords {
     public static void main(String[] args) {
@@ -20,17 +21,17 @@ public class LoanProcessing extends BankRecords {
         ResultSet rs = dao.retrieveRecords();
         System.out.println("\nPrinting out entries to console: \n\n");
         try {
-            System.out.printf("%4s\t\t%4s\t\t%6s\n", "ID:", "INCOME:", "PEP:");
+            System.out.printf("%4s\t\t \t%4s %10s\n", "ID:", "INCOME:", "PEP:");
             System.out.println("=====================================================");
             while (rs.next()) {
-                System.out.printf("%4s\t\t%4d\t\t%6s\n", rs.getString(1), rs.getInt(2),
+                System.out.printf("%4s\t\t \t%4s %10s\n", rs.getString(1), rs.getDouble(2),
                         rs.getString(3)); // get the first col, second col and then third column
                 rowData[count][0] = rs.getString(1);
-                rowData[count][1] = String.valueOf(rs.getInt(2));
+                rowData[count][1] = String.valueOf(rs.getDouble(2));
                 rowData[count][2] = rs.getString(3);
                 count += 1;
             }
-            System.out.println("\n\nAll 600 entries have been succesfully completed\n");
+            System.out.println("\n\nAll 600 entries have been succesfully retrieved\n");
             System.out.println("The output has been sorted by PREP in DESCENDING order \n");
             rs.close();
 
